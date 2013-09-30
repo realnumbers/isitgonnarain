5006d2a2ee2442bd58fa39694775df7663e623c5
Julian Sparber <julian@sparber.net>
2013-09-30 20:59:34 +0200

    new api Url, cleand up js

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@ var header = {
         "title": "Dry"
     },
     "rain": {
-        "1": "Dude, it's totally gonna rain today!",
-        "2": "Brace yourselves, Rain is coming.",
-        "3": "Rain. Rain everywhere.",
-        "4": "Bad news, guys. It's gonna rain."
+        "0": "Dude, it's totally gonna rain today!",
+        "1": "Brace yourselves, Rain is coming.",
+        "2": "Rain. Rain everywhere.",
+        "3": "Bad news, guys. It's gonna rain."
     },
     "cloudy": {
-        "1": "It might be raining today, but I'm not sure.",
-        "2": "It could possibly rain today.",
-        "3": "The possibily of rain is not to be ruled out.",
-        "4": "It may very well rain today."
+        "0": "It might be raining today, but I'm not sure.",
+        "1": "It could possibly rain today.",
+        "2": "The possibily of rain is not to be ruled out.",
+        "3": "It may very well rain today."
     },
     "sun": {
-        "1": "Nothing to worry about.",
-        "2": "Everything's fine.",
-        "3": "Lookin' good!",
-        "4": "You're in luck."
+        "0": "Nothing to worry about.",
+        "1": "Everything's fine.",
+        "2": "Lookin' good!",
+        "3": "You're in luck."
     },
     "about": {
         "title": "About"
@@ -33,22 +33,22 @@ var header = {
         "logo": ""
 		},
         "rain": {
-            "1": "Better get an umbrella or something.",
-            "2": "I suggest staying at home and chilling out today.",
-            "3": "If you're thinking of leaving the house, you best think again.",
-            "4": "You should probably not leave the house without a jacket."
+            "0": "Better get an umbrella or something.",
+            "1": "I suggest staying at home and chilling out today.",
+            "2": "If you're thinking of leaving the house, you best think again.",
+            "3": "You should probably not leave the house without a jacket."
         },
         "cloudy": {
-            "1": "Take an umbrella, or don't. Your call. Don't blame me if you get wet, though.",
-            "2": "It's hard to say whether it's goint to rain, so you better be prepared.",
-            "3": "You should probably take an umbrella with you, just to be safe.",
-            "4": "If you're leaving the house you should take an umbrella with you."
+            "0": "Take an umbrella, or don't. Your call. Don't blame me if you get wet, though.",
+            "1": "It's hard to say whether it's goint to rain, so you better be prepared.",
+            "2": "You should probably take an umbrella with you, just to be safe.",
+            "3": "If you're leaving the house you should take an umbrella with you."
         },
         "sun": {
-            "1": "No rain in sight.",
-            "2": "Rain is really unlikely today.",
-            "3": "It's not going to rain today.",
-            "4": "I'm pretty sure it's not going to rain."
+            "0": "No rain in sight.",
+            "1": "Rain is really unlikely today.",
+            "2": "It's not going to rain today.",
+            "3": "I'm pretty sure it's not going to rain."
         },
         "about": {
         	"logo": "Designed and developed by <a href=\"http://julian.sparber.net\">Julian Sparber</a> and <a href=\"http://tobiasbernard.com\">Tobias Bernard</a> for the 2013 Open Data Hackathon.<br> <br>Weather Data by <a href=\"http://openweathermap.com\">OpenWeatherMap</a>.<br> <br>This application is <a href=\"https://www.gnu.org/philosophy/free-sw.html\">Free Software</a>, released under the <a href=\"https://www.gnu.org/licenses/gpl.html\">GPLv3</a> or later.<br>Get the source on <a href=\"https://github.com/realnumbers/isitgonnarain\">Github</a>."
@@ -56,9 +56,11 @@ var header = {
 };	
 	var RAIN = 0, CLOUDY = 1, SUN = 2, ERR0R = 3, ABOUT = 4, BLANK = 5;
 	var slides = ["rain", "cloudy", "sun", "error", "about", "blank"];
+	var numberOfSlides = 6;
 	var rainCt = 0, 
 		cloudyCt = 0
 		sunCt = 0;
+	var city, country; 
 	var nText, nSub, nIcon;
 	var previousPage = BLANK, 
 		beforePage = BLANK;
@@ -95,85 +97,43 @@ var header = {
 	// to get the location name and weather forecast
 	function locationSuccess(position) {
 		try{
-			// Retrive the cache
-			var cache = localStorage.weatherCache && JSON.parse(localStorage.weatherCache);
-			var d = new Date();
-			// If the cache is newer than 30 minutes, use the cache
-			if(cache && cache.timestamp && cache.timestamp > d.getTime() - 30*60*1000){
-				// Get the offset from UTC (turn the offset minutes into ms)
-				var offset = d.getTimezoneOffset()*60*1000;
-				var city = cache.data.city.name;
-				var country = cache.data.city.country;
-				$.each(cache.data.list, function(){
-					// "this" holds a forecast object
-					// Get the local time of this forecast (the api returns it in utc)
-					var localTime = new Date(this.dt*1000 - offset);
-					var sum = sunCt + cloudyCt + rainCt;
-					if (sum > 3){
-						determineSlide();
-						return false;
-					}
-					else {
-						checkForecast(this.weather[0].main);
-					}
+				function getWeather(callback) {
+				var weather = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&units=metric&cnt=1';
+				$.ajax({
+				  dataType: "jsonp",
+				  url: weather,
+				  success: callback
 				});
-				// Add the location to the page
+			}
+
+			// get data:
+			getWeather(function (data) {
+				console.log('weather data received');
+				console.log(data.list[0].weather[0].main);
+				city = data.city.name;
+				country = data.city.country;
 				location.html(city+', <b>'+country+'</b>');
+				changeSlide(data.list[0].weather[0].main);
+				
+			});
+
 				weatherDiv.addClass('loaded');
 				// Set the slider to the first slide
-			}
-			else{
-				// If the cache is old or nonexistent, issue a new AJAX request
-				var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?lat='+position.coords.latitude+
-									'&lon='+position.coords.longitude+'&callback=?'
-				$.getJSON(weatherAPI, function(response){
-					// Store the cache
-					localStorage.weatherCache = JSON.stringify({
-						timestamp:(new Date()).getTime(),	// getTime() returns milliseconds
-						data: response
-					});
-					// Call the function again
-					locationSuccess(position);
-				});
-			}
+
 		}
+		
 		catch(e){
 			/*Error:We can't find information about your city!*/
+			alert("Error");
 			locationError("NO_CITY_FOUND");
 			window.console && console.error(e);
 		}
 	}
-	function checkForecast(condition){
-		if (condition === "Clear"){
-			sunCt++;
-		}
-		else {
-			if (condition === "Clouds"){
-				cloudyCt++;
-			}
-			else {
-				rainCt++;
-			}
-		}
-	}
-	function determineSlide(){
-		if (rainCt > 2){
-			changeSlide(0);
-		}
-		else { 
-			if (sunCt + cloudyCt > 2){
-				changeSlide(2);
-			}
-			else {
-				changeSlide(1);
-			}
-		}
-		resetCt();
-	}
-	function changeSlide(slideNr){
+
+	function changeSlide(condition){
 		//randomGen();
-		var slide = document.getElementById("slide-" + slides[slideNr]);
-		for (var i = 0; i < 6; i++) {
+		var slide = document.getElementById("slide-" + condition);
+		for (var i = 0; i < numberOfSlides; i++) {
 			var s = document.getElementById("slide-" + slides[i]);
 			s.style.display="none";
 		} 
@@ -215,7 +175,7 @@ var header = {
 	}
 	// Gernerate Random Number
 	function randomGen() {
-		return Math.floor((Math.random()*4)+1);
+		return Math.floor((Math.random()* (numberOfSlides - 2)));
 	}
 	//Change Slide to About Page
 	function aboutPage(){
